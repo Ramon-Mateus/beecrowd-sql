a1396b098d880d89d348010a93b44a3bb26c55b9
Ramon de Oliveira Araújo Mateus <60753390+Ramon-Mateus@users.noreply.github.com>
2023-09-09 11:11:03 -0300

    Update 2611.sql

--- 2611.sql
+++ 2611.sql
@@ -1,3 +1,9 @@
-select movies.id, movies.name from movies
-    inner join genres on (movies.id_genres = genres.id)
-        where genres.description = 'Action';
+-- Usando INNER JOIN
+-- select movies.id, movies.name from movies
+--    inner join genres on (movies.id_genres = genres.id)
+--        where genres.description = 'Action';
+
+-- Usando subconsulta
+select movies.id, movies.name from movies where movies.id_genres = (
+    select id from genres where description = 'Action'
+);
